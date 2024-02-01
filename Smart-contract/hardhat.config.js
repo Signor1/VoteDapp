0c59d2582a331c927b65fa5bfbbe4a89e1febca8
coderofphcity <agantemboy@gmail.com>
2024-02-01 14:59:46 +0100

    added sepolia network

--- Smart-contract/hardhat.config.js
+++ Smart-contract/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia : {
+       url: process.env.alchemyUrl,
+       accounts: [process.env.pvky],
+     },
+   },
+   
+   etherscan:{
+    apiKey:process.env.ETHscanAPI
+   }
 };
