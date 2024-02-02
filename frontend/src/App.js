52b04c4e2568cfa43da33ca16520237635b41048
Olorunsogo Banwo <sogobanwo@gmail.com>
2024-02-02 16:31:24 +0100

    Feat: Added the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Field, Form, Formik } from "formik";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   return (
+  <div className="container w-[80%] flex flex-col justify-center items-center mx-auto mt-[15%] py-12 px-6 border border-solid border-veryLightgrey rounded-lg md:w-[450px] md:mt-[5%] ">
+    <div>
+      <h1 className="text-3xl font-bold flex justify-center font-700 font-WorkSans">
+        Hi, Welcome
+      </h1>
+      <p className="text-lightGrey text-center font-WorkSans">
+        Vote for your preferred Candidate
+      </p>
     </div>
-  );
-}
+    <Formik
+      initialValues={{
+        picked: "",
+      }}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      {({ values }) => (
+        <Form>
+          <div role="group" aria-labelledby="my-radio-group" className="flex flex-col">
+            <label className="my-2 flex gap-4">
+              <Field type="radio" name="picked" value="One" />
+              Candidate 1
+            </label>
+            <label className="my-2 flex gap-4">
+              <Field type="radio" name="picked" value="Two" />
+              Candidate 2
+            </label>
+            <label className="my-2 flex gap-4">
+              <Field type="radio" name="picked" value="Three" />
+              Candidate 3
+            </label>
+          </div>
+          <button
+            type="submit"
+            // disabled={isSubmitting}
+            className="text-white bg-black rounded-md p-1 md:p-1.5 font-Inter w-full mt-2"
+          >
+            Cast Vote
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+)}
 
-export default App;
+export default App
